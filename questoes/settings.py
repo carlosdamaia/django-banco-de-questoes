e5fb0610bbb929693e63792baee88f394c0b502a
Ubuntu <carlosemaya@gmail.com>
2024-03-19 11:38:50 +0000

    config.json

--- questoes/settings.py
+++ questoes/settings.py
@@ -15,7 +15,7 @@ from dotenv import load_dotenv
 import os
 import json
 
-with open('/etc/config.json') as config_file:
+with open('config.json') as config_file:
     config = json.load(config_file)
 
 load_dotenv()
